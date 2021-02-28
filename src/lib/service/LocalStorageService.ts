a9628de697ab5b2f82efa9290d93c1df4edbb74f
Jeontaeyun <jow1813@naver.com>
2021-02-28 17:39:52 +0900

    feat(localStorageService): implement local storage wrapper for roledex

---
+++ src/lib/service/LocalStorageService.ts
@@ -0,0 +1,121 @@
+import { v1 } from "uuid";
+
+import { ContactType, ContextInputType } from "../../types/contact";
+
+enum LOCAL_STORAGE_KEY {
+  CONTACT = "CONTACT",
+  CONTACT_LIST = "CONTACT_LIST",
+  FAVORITE_CONTACT_LIST = "FAVORITE_CONTACT_LIST",
+}
+
+class LocalStorageService {
+  public isLoaded = false;
+  public isLoading = false;
+  private static _instance: LocalStorageService;
+
+  private constructor() {
+    LocalStorageService._instance = this;
+  }
+
+  public static get shared() {
+    if (!LocalStorageService._instance) {
+      LocalStorageService._instance = new LocalStorageService();
+    }
+    return LocalStorageService._instance;
+  }
+
+  public init = () => {
+    this.isLoading = true;
+    const contactList: string[] = this.parseJSON(localStorage.getItem(LOCAL_STORAGE_KEY.CONTACT_LIST), []);
+    const favoriteContactList: string[] = this.parseJSON(
+      localStorage.getItem(LOCAL_STORAGE_KEY.FAVORITE_CONTACT_LIST),
+      []
+    );
+
+    return { contactList, favoriteContactList };
+  };
+
+  public getContactWithId = (id: string): ContactType | null => {
+    const contact: ContactType | null = this.parseJSON(localStorage.getItem(LOCAL_STORAGE_KEY.CONTACT + id), null);
+    return contact;
+  };
+
+  public createContactWithId = (data: ContextInputType): ContactType => {
+    const id = v1();
+    const newContact = { id, ...data };
+    const stringifiedContact = JSON.stringify(newContact);
+
+    localStorage.setItem(LOCAL_STORAGE_KEY.CONTACT + id, stringifiedContact);
+    this.addContactToList(id);
+
+    return newContact;
+  };
+
+  public updateContactWithId = (id: string, data: ContextInputType): ContactType => {
+    const updatedContact = { id, ...data };
+    const stringifiedContact = JSON.stringify(updatedContact);
+
+    localStorage.setItem(LOCAL_STORAGE_KEY.CONTACT + id, stringifiedContact);
+    return updatedContact;
+  };
+
+  public removeContactWithId = (id: string): boolean => {
+    localStorage.removeItem(LOCAL_STORAGE_KEY + id);
+
+    this.removeContactFromList(id);
+    this.removeFavorite(id);
+
+    return true;
+  };
+
+  public getContactList = (): string[] => {
+    return this.parseJSON(localStorage.getItem(LOCAL_STORAGE_KEY.CONTACT_LIST), []);
+  };
+
+  public setContactList = (contactList: string[]): string[] => {
+    const stringifiedContactList = JSON.stringify(contactList);
+    localStorage.setItem(LOCAL_STORAGE_KEY.CONTACT_LIST, stringifiedContactList);
+
+    return contactList;
+  };
+
+  public addContactToList = (id: string) => {
+    const previousContactList = this.getContactList();
+    this.setContactList([id, ...previousContactList]);
+  };
+
+  public removeContactFromList = (id: string) => {
+    const previousContactList = this.getContactList();
+    const newContactList = previousContactList.filter((item) => item === id);
+    this.setContactList(newContactList);
+  };
+
+  public getFavoriteContactList = (): string[] => {
+    return this.parseJSON(localStorage.getItem(LOCAL_STORAGE_KEY.FAVORITE_CONTACT_LIST), []);
+  };
+
+  public setFavoriteContactList = (contactList: string[]): string[] => {
+    const stringifiedContactList = JSON.stringify(contactList);
+    localStorage.setItem(LOCAL_STORAGE_KEY.FAVORITE_CONTACT_LIST, stringifiedContactList);
+
+    return contactList;
+  };
+
+  public addFavorite = (id: string) => {
+    const previousFavoriteContactList = this.getFavoriteContactList();
+    this.setFavoriteContactList([id, ...previousFavoriteContactList]);
+  };
+
+  public removeFavorite = (id: string) => {
+    const previousFavoriteContactList = this.getFavoriteContactList();
+    const newFavoriteContactList = previousFavoriteContactList.filter((item) => item === id);
+    this.setFavoriteContactList(newFavoriteContactList);
+  };
+
+  private parseJSON = (string: string | null, defaultValue: any = null) => {
+    if (string) return JSON.parse(string);
+    else return defaultValue;
+  };
+}
+
+export default LocalStorageService;
---
+++ src/types/contact/index.ts
@@ -0,0 +1,12 @@
+export type ContactType = {
+  id: string;
+  name: string;
+  email: string;
+  phoneNumber?: string;
+};
+
+export type ContextInputType = {
+  name: string;
+  email: string;
+  phoneNumber?: string;
+};
